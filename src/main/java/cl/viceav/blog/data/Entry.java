5b690afceb23db98f58087ac1e2811da2a9738fa
viceav <vicentealamosv@gmail.com>
2024-12-25 17:18:08 -0300

    feat: update Entry entity with additional fields and annotations
    - Add @GeneratedValue annotation for id field
    - Rename name field to fileName
    - Add title, modified, and modified_at fields
    - Change created_at field type to ZonedDateTime

--- src/main/java/cl/viceav/blog/data/Entry.java
+++ src/main/java/cl/viceav/blog/data/Entry.java
@@ -1,32 +1,68 @@
 package cl.viceav.blog.data;
 
 import java.sql.Timestamp;
+import java.time.ZonedDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Entry {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
   @NotNull
   private String route;
 
   @NotNull
-  private String name;
+  private String fileName;
 
+  @NotNull
+  private String title;
+
+  @NotNull
   @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-  private Timestamp created_at;
+  private ZonedDateTime created_at;
+
+  private Boolean modified;
+
+  private Timestamp modified_at;
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Boolean getModified() {
+    return modified;
+  }
+
+  public void setModified(Boolean modified) {
+    this.modified = modified;
+  }
+
+  public Timestamp getModified_at() {
+    return modified_at;
+  }
+
+  public void setModified_at(Timestamp modified_at) {
+    this.modified_at = modified_at;
+  }
 
   public void setRoute(String route) {
     this.route = route;
   }
 
-  public void setName(String name) {
-    this.name = name;
+  public void setFileName(String name) {
+    this.fileName = name;
   }
 
   public int getId() {
@@ -37,11 +73,11 @@ public class Entry {
     return route;
   }
 
-  public String getName() {
-    return name;
+  public String getFileName() {
+    return fileName;
   }
 
-  public Timestamp getCreated_at() {
+  public ZonedDateTime getCreated_at() {
     return created_at;
   }
 }
